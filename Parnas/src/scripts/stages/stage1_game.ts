a11f1366d899674b99deb6c30242a5565db524f0
4workacc <58693140+4workacc@users.noreply.github.com>
2023-03-29 21:11:46 +0300

    feat: some stage1_map

--- Parnas/src/scripts/stages/stage1_game.ts
+++ Parnas/src/scripts/stages/stage1_game.ts
@@ -1,5 +1,27 @@
 import "../../styles/_stage1.scss";
+import { IMan } from "../tasks/task1";
+import { stage1Authors } from "../tasks/task1";
+
+const stage1_countOfAuthors: number = 5;
+let stage1_arrayOfAuthors: IMan[] = [];
+let stage1_arrayOfMarks: HTMLElement[] = [];
 
 export const stage1_StartGame = () => {
-    
+    stage1_generateArrayOfAuthors();
+    console.log(1, stage1_arrayOfAuthors);
+}
+
+const stage1_generateArrayOfAuthors = () => {
+    while (stage1_arrayOfAuthors.length < stage1_countOfAuthors ) {
+        let randInd: number = Math.floor(Math.random()*stage1Authors.length);
+        if ( stage1_arrayOfAuthors.indexOf(stage1Authors[randInd]) === -1) {
+            stage1_arrayOfAuthors.push(stage1Authors[randInd]);
+            let newMark: HTMLElement = document.createElement("div");
+            newMark.classList.add("Stage1_Mark");
+            newMark.style.left = `${stage1Authors[randInd].coordX}px`;
+            newMark.style.top = `${stage1Authors[randInd].coordY}px`;
+            newMark.title = `${stage1Authors[randInd].address}`;
+            document.getElementById("Stage1_Map")?.appendChild(newMark);
+        }
+    }   
 }
--- Parnas/src/scripts/stages/stage1_navigation.ts
+++ Parnas/src/scripts/stages/stage1_navigation.ts
@@ -1,4 +1,5 @@
 import "../../styles/_stage1.scss";
+import { stage1_StartGame } from "./stage1_game";
 
 let stage1_score:number = 0;
 
@@ -6,16 +7,18 @@ let stage1_Title_Screen: HTMLElement = document.getElementById("Stage1_TitleScre
 let stage1_Game_Screen: HTMLElement = document.getElementById("Stage1_Game")!;
 let stage1_Result_screen: HTMLElement = document.getElementById("Stage1_Result")!;
 
+
 const stage1_Start = () => {
     stage1_Title_Screen.style.display = "none";
-    stage1_Game_Screen.style.display = "block";
+    stage1_Game_Screen.style.display = "block";   
 }
 
 let stage1_Title_but = document.getElementById("Stage1_Title_but")!;
 
 stage1_Title_but.onclick = () => {
     stage1_Title_Screen.style.display = "none";
-    stage1_Result_screen.style.display = "block";
+    stage1_Game_Screen.style.display = "block";
+    stage1_StartGame();
 }
 
 let stage1_Game_but = document.getElementById("Stage1_Game_but");
